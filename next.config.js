81f5d60db06d0c1427ef674cb72cb594ad08ea08
panselbumd <pembinaan.bumdkwb@gmail.com>
2026-06-11 18:58:54 +0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { serverActions: { allowedOrigins: ['localhost:3000'] } },
-  images: {
-    remotePatterns: [{ protocol: 'https', hostname: '*.supabase.co' }],
-  },
-}
-
-module.exports = nextConfig
